43b41a2fc7ed7097a8f819b8121bbc7fef04e534
Dominik Klapuch <klapuchdominik@gmail.com>
2019-12-28 02:40:14 +0100

    Without use of database

--- db/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE results (
-	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	page text NOT NULL,
-	section text NOT NULL,
-	selector text NOT NULL,
-	stored_at timestamptz NOT NULL DEFAULT now()
-);
--- src/main.clj
+++ src/main.clj
@@ -1,14 +1,20 @@
 (ns ktb.core
   (:require [clj-http.client :as client])
-  (:require [clojure.java.jdbc :as jdbc])
   (:require [digest])
   (:require [cheshire.core :as json])
   (:import [org.jsoup Jsoup])
+  (:require [clojure.java.io :as io])
   (:require [clojure.string :as str]))
 
 (def config (load-file "config/config.local.edn"))
 
-(def pg-db (get config :db))
+(def KAKTUS_SECTION_FILENAME 
+  (let [filename (format "%s/bonuses" (get (get config :store) :filename))]
+    (if-not (.exists (io/file filename))
+      (do
+        (io/make-parents filename)
+        (spit filename "")))
+    filename))
 
 (defn with-telegram-token [path]
   (let [TELEGRAM_URL "https://api.telegram.org"]
@@ -25,21 +31,17 @@
 
 (defn unify-section [section] (digest/md5 (str/trim section)))
 
-; There should be repeatedle-read transaction, but it somehow does not work..
-(def last-section
-  (let [row (jdbc/query pg-db ["SELECT section FROM results ORDER BY id DESC LIMIT 1"])]
-    (get (first row) :section)))
+(def last-section (slurp KAKTUS_SECTION_FILENAME))
+(spit KAKTUS_SECTION_FILENAME kaktus-section)
 
-(jdbc/insert! pg-db :results {:page kaktus-page :section kaktus-section :selector KAKTUS_SELECTOR})
-
-(def subsriber-ids
+(def subscriber-ids
   (let [updates (get (client/get (with-telegram-token "getUpdates")) :body)]
     (map (fn [result] (get (get (get result :message) :from) :id))
          (filter (fn [result] (= "/start" (str/trim (get (get result :message) :text))))
                  (get (json/parse-string updates true) :result)))))
 
 (defn send-hook! []
-  (doseq [chat_id subsriber-ids]
+  (doseq [chat_id subscriber-ids]
     (client/post
      (with-telegram-token "sendMessage")
      {:query-params {:chat_id chat_id :text kaktus-section}})))
