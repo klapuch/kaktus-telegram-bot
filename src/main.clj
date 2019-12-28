4be69ac5a6d9d6eba5650ec53e8ba1fff583d91c
Dominik Klapuch <klapuchdominik@gmail.com>
2019-12-28 16:14:02 +0100

    Storing already subscribed users

--- src/main.clj
+++ src/main.clj
@@ -4,21 +4,37 @@
   (:require [cheshire.core :as json])
   (:import [org.jsoup Jsoup])
   (:require [clojure.java.io :as io])
+  (:require [clojure.data :as data])
   (:require [clojure.string :as str]))
 
 (def config (load-file "config/config.local.edn"))
 
+(defn create-file-if-not-exists
+  [filename]
+  (if-not (.exists (io/file filename))
+    (do
+      (io/make-parents filename)
+      (spit filename "")))
+  filename)
+
+(defn with-store-directory
+  [path]
+  (format "%s/%s" (get (get config :store) :filename) path))
+
 (def KAKTUS_SECTION_FILENAME 
-  (let [filename (format "%s/bonuses" (get (get config :store) :filename))]
-    (if-not (.exists (io/file filename))
-      (do
-        (io/make-parents filename)
-        (spit filename "")))
+  (let [filename (with-store-directory "bonuses")]
+    (create-file-if-not-exists filename)
     filename))
 
-(defn with-telegram-token [path]
-  (let [TELEGRAM_URL "https://api.telegram.org"]
-    (format "%s/bot%s/%s" TELEGRAM_URL (get (get config :telegram) :token) path)))
+(def KAKTUS_SUBSCRIBERS_FILENAME
+  (let [filename (with-store-directory "subscribers")]
+    (create-file-if-not-exists filename)
+    filename))
+
+(defn with-telegram-token
+  [path]
+  (let [telegram_url "https://api.telegram.org"]
+    (format "%s/bot%s/%s" telegram_url (get (get config :telegram) :token) path)))
 
 (def KAKTUS_URL "https://www.mujkaktus.cz/homepage")
 (def KAKTUS_SELECTOR ".box-bubble > * > .journal-content-article:nth-of-type(2) > * > p:not(:empty):nth-of-type(2)")
@@ -34,11 +50,17 @@
 (def last-section (slurp KAKTUS_SECTION_FILENAME))
 (spit KAKTUS_SECTION_FILENAME kaktus-section)
 
-(def subscriber-ids
+(def new-subscriber-ids
   (let [updates (get (client/get (with-telegram-token "getUpdates")) :body)]
     (map (fn [result] (get (get (get result :message) :from) :id))
          (filter (fn [result] (= "/start" (str/trim (get (get result :message) :text))))
                  (get (json/parse-string updates true) :result)))))
+
+(def current-subscriber-ids
+  (str/split (slurp KAKTUS_SUBSCRIBERS_FILENAME) #"\n"))
+
+(spit KAKTUS_SUBSCRIBERS_FILENAME (str/join "\n" (filter some? (nth(data/diff new-subscriber-ids current-subscriber-ids) 0))))
+(def subscriber-ids (distinct (into new-subscriber-ids current-subscriber-ids)))
 
 (defn send-hook! []
   (doseq [chat_id subscriber-ids]
